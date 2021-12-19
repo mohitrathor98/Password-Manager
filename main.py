83a2b6a501e40718134f304b7f244b80df3633bf
mohitrathor98 <mohitrathor8@gmail.com>
2021-12-19 10:59:09 +0530

    GUI setup complete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter.constants import LEFT
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -8,17 +9,40 @@ import tkinter
 window = tkinter.Tk()
 
 window.title("Password Manager")
-window.minsize(width=300, height=300)
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
+# logo
 canvas = tkinter.Canvas(width=200, height=200)
 tomato_img = tkinter.PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=tomato_img)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+# labels
+website_label = tkinter.Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+email_label = tkinter.Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+password_label = tkinter.Label(text= "Password:")
+password_label.grid(column=0, row=3)
 
 
+# inputs
+website_input = tkinter.Entry(width=40)
+website_input.grid(column=1, row=1, columnspan=2)
 
+email_input = tkinter.Entry(width=40)
+email_input.grid(column=1, row=2, columnspan=2)
 
+password_input = tkinter.Entry(width=21)
+password_input.grid(column=1, row=3)
 
+# buttons
+generate_password = tkinter.Button(text="Generate Password")
+generate_password.grid(column=2, row=3)
+
+add_button = tkinter.Button(text="Add", width=38)
+add_button.grid(column=1, row=4, columnspan=2, pady=5)
 
 window.mainloop()
